4b3d8d4056435a3189ae459ea9f16db4a394ff49
go-lover <3490755245@qq.com>
2022-02-02 15:52:35 +0800

    验证手机号是否注册

--- routes/api.go
+++ routes/api.go
@@ -1,15 +1,23 @@
+// Package routes 注册路由
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"gohub/app/http/controllers/api/v1/auth"
+
+	"github.com/gin-gonic/gin"
+)
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
-	v1 := r.Group("v1")
+
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
 	{
-		v1.GET("/hello", func(c *gin.Context) {
-			c.JSON(200,"hello")
-
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
-
 }
