2d4c970bddbad314bc4652258fc35e64d212f372
xiaoraocoding <3490755245@qq.com>
2022-04-18 19:35:58 +0800

    封装邮箱验证功能

--- config/mail.go
+++ config/mail.go
@@ -9,7 +9,7 @@ func init() {
 
 			// 默认是 Mailhog 的配置
 			"smtp": map[string]interface{}{
-				"host":     config.Env("MAIL_HOST", "localhost"),
+				"host":     config.Env("MAIL_HOST", "127.0.0.1"),
 				"port":     config.Env("MAIL_PORT", 1025),
 				"username": config.Env("MAIL_USERNAME", ""),
 				"password": config.Env("MAIL_PASSWORD", ""),
